66e4b2de1e82cb355855bd9e5f4ca8dbd8b2b2e1
“Ramil <ramil.sharapov1988@gmail.com>
2023-12-04 23:37:55 -0400

    some significant changes

--- src/App.js
+++ src/App.js
@@ -13,17 +13,21 @@ import {
   SkeletonText,
   CloseButton,
   useClipboard,
-  Flex
+  Flex,
 } from "@chakra-ui/react";
 import { FaChevronCircleDown } from "react-icons/fa";
 import { ChakraProvider } from "@chakra-ui/react";
+import { diffWords } from "diff";
 
 function RewRitter() {
   const [text, setText] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  const [errorHighlights, setErrorHighlights] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [numSuggestions, setNumSuggestions] = useState(1);
   const { hasCopied, onCopy } = useClipboard(suggestions.join("\n"));
+  const [highlightedText, setHighlightedText] = useState("");
+  const [hasCopiedText, setHasCopiedText] = useState(false);
 
   const handleInputChange = (e) => {
     setText(e.target.value);
@@ -31,7 +35,9 @@ function RewRitter() {
 
   const handleSubmit = async () => {
     setIsLoading(true);
-    let instruction = `Please provide ${numSuggestions} separate suggestions for rewriting the following text: ${text}`;
+    setErrorHighlights(""); // Clear error highlights
+
+    let instruction = `Please provide ${numSuggestions} separate suggestions for rewriting the following text: "${text}"`;
 
     try {
       const response = await fetch(
@@ -65,16 +71,65 @@ function RewRitter() {
       }
 
       const data = await response.json();
-      const rawSuggestions = data.choices[0].message.content.split("\n");
-      const filteredSuggestions = rawSuggestions.filter(
-        (suggestion) => suggestion.trim() !== ""
+      const rawSuggestions = data.choices[0].message.content.trim().split("\n");
+      const filteredSuggestions = rawSuggestions
+        .filter((suggestion) => suggestion.trim() !== "")
+        .map(
+          (suggestion) =>
+            suggestion
+              .replace(/^\d+\.\s*/, "") // Remove leading numbers and dots
+              .replace(/^"|"$/g, "") // Remove leading and trailing quotes
+        );
+
+      setSuggestions(filteredSuggestions.slice(0, numSuggestions));
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
+
+  const handleCheckErrors = async () => {
+    setIsLoading(true);
+    setSuggestions([]);
+
+    let instruction = `Please rewrite the following text with corrections: "${text}"`;
+
+    try {
+      const response = await fetch(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${OPENAI_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+              {
+                role: "system",
+                content: "You are a helpful assistant.",
+              },
+              {
+                role: "user",
+                content: instruction,
+              },
+            ],
+          }),
+        }
       );
 
-      const processedSuggestions = filteredSuggestions.map((suggestion) =>
-        suggestion.replace(/^\d+\.\s*/, "").replace(/^"|"$/g, "")
-      );
+      if (!response.ok) {
+        const responseData = await response.json();
+        console.error("OpenAI API Error:", responseData);
+        throw new Error(`OpenAI API responded with status: ${response.status}`);
+      }
 
-      setSuggestions(processedSuggestions.slice(0, numSuggestions));
+      const data = await response.json();
+      const correctedText = data.choices[0].message.content;
+      const highlightedHtml = applyDiff(text, correctedText);
+      setHighlightedText(highlightedHtml);
 
       setIsLoading(false);
     } catch (error) {
@@ -88,7 +143,45 @@ function RewRitter() {
   };
 
   const clearText = () => {
-    setText(""); 
+    setText("");
+    setSuggestions([]);
+    setErrorHighlights("");
+  };
+
+  const copyTextContent = () => {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = highlightedText;
+    let textToCopy = "";
+
+    Array.from(tempDiv.childNodes).forEach((node) => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        const element = node;
+        if (!element.classList.contains("removed")) {
+          textToCopy += element.textContent;
+        }
+      } else if (node.nodeType === Node.TEXT_NODE) {
+        textToCopy += node.textContent;
+      }
+    });
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      setHasCopiedText(true);
+      setTimeout(() => setHasCopiedText(false), 2000);
+    });
+  };
+
+  const applyDiff = (oldText, newText) => {
+    const diffResult = diffWords(oldText, newText);
+    return diffResult
+      .map((part) => {
+        const highlightClass = part.added
+          ? "added"
+          : part.removed
+          ? "removed"
+          : "";
+        return `<span class="${highlightClass}">${part.value}</span>`;
+      })
+      .join("");
   };
 
   return (
@@ -96,63 +189,98 @@ function RewRitter() {
       <OuterContainer>
         <AppContainer>
           <Box textAlign="center" py={10} color="white">
-            <FancyHeading mb={7}>Rewrite perfect with AI</FancyHeading>
-            <Menu>
-              <MenuButton as={Button} rightIcon={<FaChevronCircleDown />}>
-                Number of Suggestions: {numSuggestions}
-              </MenuButton>
-              <MenuList>
-                {[1, 2, 3].map((number) => (
-                  <MenuItem
-                    key={number}
-                    color="black"
-                    onClick={() => setNumSuggestions(number)}
+            <FancyHeading mb={7}>Write Perfect with AI</FancyHeading>
+            <Flex direction="column" mb={4}>
+              <Textarea
+                bg="white"
+                color="black"
+                value={text}
+                onChange={handleInputChange}
+                placeholder="Enter your text"
+                size="lg"
+              />
+              <Button
+                colorScheme="red"
+                size="sm"
+                alignSelf="flex-end"
+                mt={2}
+                onClick={clearText}
+              >
+                Clear
+              </Button>
+            </Flex>
+            <Flex justifyContent="space-between" alignItems="center" mb={4}>
+              <Flex alignItems="center">
+                <Button
+                  colorScheme="blue"
+                  ml={4}
+                  onClick={handleSubmit}
+                  isDisabled={!text}
+                >
+                  Rewrite Text times:
+                </Button>
+                <Menu>
+                  <MenuButton
+                    ml={1}
+                    as={Button}
+                    rightIcon={<FaChevronCircleDown />}
                   >
-                    {number}
-                  </MenuItem>
-                ))}
-              </MenuList>
-            </Menu>
-            <Box my={4}>
-  <Flex flexDirection="column" alignItems="flex-end">
-    <Textarea
-      bg="white"
-      color="black"
-      value={text}
-      onChange={handleInputChange}
-      placeholder="Enter your text"
-      size="lg"
-    />
-    <Button
-      colorScheme="red"
-      size="sm"
-      mt={2}
-      onClick={clearText}
-    >
-      Clear Text
-    </Button>
-  </Flex>
-  <Button
-    colorScheme="blue"
-    my={5}
-    onClick={handleSubmit}
-    isDisabled={!text}
-  >
-    Get back from AI
-  </Button>
-</Box>
+                    {numSuggestions}
+                  </MenuButton>
+                  <MenuList>
+                    {[1, 2, 3].map((number) => (
+                      <MenuItem
+                        key={number}
+                        color="black"
+                        onClick={() => setNumSuggestions(number)}
+                      >
+                        {number}
+                      </MenuItem>
+                    ))}
+                  </MenuList>
+                </Menu>
+                <Button
+                  colorScheme="green"
+                  ml={4}
+                  onClick={handleCheckErrors}
+                  isDisabled={!text}
+                >
+                  Check Errors
+                </Button>
+              </Flex>
+            </Flex>
             {isLoading ? (
               <SkeletonText mt="4" noOfLines={4} spacing="4" />
             ) : (
-              suggestions.map((suggestion, index) => (
-                <SuggestionBox key={index}>
-                  {suggestion}
-                  <Button onClick={onCopy} colorScheme="blue" size="sm" m={2}>
-                    {hasCopied ? "Copied" : "Copy"}
-                  </Button>
-                  <StyledCloseButton onClick={() => removeSuggestion(index)} />
-                </SuggestionBox>
-              ))
+              <Box>
+                {highlightedText && (
+                  <HighlightedText>
+                    <div
+                      dangerouslySetInnerHTML={{ __html: highlightedText }}
+                    />
+                    <Button
+                      onClick={copyTextContent}
+                      colorScheme="blue"
+                      size="sm"
+                      m={2}
+                    >
+                      {hasCopiedText ? "Copied" : "Copy Text"}
+                    </Button>
+                    <StyledCloseButton onClick={() => setHighlightedText("")} />
+                  </HighlightedText>
+                )}
+                {suggestions.map((suggestion, index) => (
+                  <SuggestionBox key={index}>
+                    {suggestion}
+                    <Button onClick={onCopy} colorScheme="blue" size="sm" m={2}>
+                      {hasCopied ? "Copied" : "Copy"}
+                    </Button>
+                    <StyledCloseButton
+                      onClick={() => removeSuggestion(index)}
+                    />
+                  </SuggestionBox>
+                ))}
+              </Box>
             )}
           </Box>
         </AppContainer>
@@ -175,7 +303,7 @@ const OuterContainer = styled.div`
 `;
 
 const AppContainer = styled.div`
-  width: 50vw;
+  width: 70vw;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -202,6 +330,28 @@ const FancyHeading = styled(Heading)`
 `;
 
 const SuggestionBox = styled(Box)`
+  width: 50%;
+  margin: 2rem auto;
+  padding: 1rem;
+  padding-right: 3rem;
+  background-color: #f7fafc;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 0.5rem;
+  position: relative;
+  color: black;
+  overflow: hidden;
+  white-space: pre-wrap;
+`;
+
+const StyledCloseButton = styled(CloseButton)`
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+`;
+
+const HighlightedText = styled.div`
+  background-color: white;
+  color: black;
   width: 80%;
   margin: 2rem auto;
   padding: 1rem;
@@ -212,12 +362,14 @@ const SuggestionBox = styled(Box)`
   position: relative;
   color: black;
   overflow: hidden;
-`;
-
-const StyledCloseButton = styled(CloseButton)`
-  position: absolute;
-  right: 1rem;
-  top: 1rem;
+  white-space: pre-wrap;
+  .added {
+    background-color: #90ee90;
+  }
+  .removed {
+    background-color: #ffcccb;
+    text-decoration: line-through;
+  }
 `;
 
 export default RewRitter;
